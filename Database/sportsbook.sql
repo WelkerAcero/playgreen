cfa3c0967378add6daa11bccf1f6c64cff2b4902
WelkerAcero <universityestudiosca@gmail.com>
2024-06-05 11:14:41 -0500

    Database/sportsbook.sql added to fill SQL

---
+++ Database/sportsbook.sql
@@ -0,0 +1,475 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-06-2024 a las 18:12:01
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sportsbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bets`
+--
+
+CREATE TABLE `bets` (
+  `id` int(11) NOT NULL,
+  `bet_option` int(11) NOT NULL,
+  `odd` double NOT NULL,
+  `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'active',
+  `result` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `event_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'WITHDRAW'),
+(2, 'DEPOSIT'),
+(3, 'BET'),
+(4, 'WIN');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'COLOMBIA'),
+(2, 'UNITED STATES');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `event_place` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `event_date` datetime(3) NOT NULL,
+  `sport_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL,
+  `type` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `type`) VALUES
+(5, 'EDIT-PROFILE'),
+(6, 'MAKE-BET'),
+(7, 'MAKE-DEPOSIT'),
+(8, 'MAKE-WITDRAW'),
+(9, 'READ-BALANCE'),
+(1, 'USER-CREATE'),
+(4, 'USER-DELETE'),
+(2, 'USER-READ'),
+(3, 'USER-UPDATE');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `rol_name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `rol_name`, `createdAt`, `updatedAt`) VALUES
+(1, 'Admin', '0000-00-00 00:00:00.000', '0000-00-00 00:00:00.000'),
+(2, 'User', '0000-00-00 00:00:00.000', '0000-00-00 00:00:00.000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rolespermissions`
+--
+
+CREATE TABLE `rolespermissions` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `rolespermissions`
+--
+
+INSERT INTO `rolespermissions` (`id`, `role_id`, `permission_id`, `createdAt`, `updatedAt`) VALUES
+(1, 1, 1, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(2, 1, 2, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(3, 1, 3, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(4, 1, 5, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(5, 1, 7, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(6, 1, 9, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(7, 2, 6, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(8, 2, 7, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(9, 2, 8, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000'),
+(10, 2, 9, '2024-06-05 11:07:22.000', '2024-06-05 11:07:22.000');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sports`
+--
+
+CREATE TABLE `sports` (
+  `id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `sports`
+--
+
+INSERT INTO `sports` (`id`, `name`) VALUES
+(2, 'Basketball'),
+(1, 'Football');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `teams`
+--
+
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `documentId` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cellphone` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` char(1) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `birthDate` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
+  `deletedAt` datetime(3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `userstransactions`
+--
+
+CREATE TABLE `userstransactions` (
+  `id` int(11) NOT NULL,
+  `amount_money` decimal(6,4) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `bet_id` int(11) DEFAULT NULL,
+  `createdAt` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `updatedAt` datetime(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `_prisma_migrations`
+--
+
+CREATE TABLE `_prisma_migrations` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `checksum` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `finished_at` datetime(3) DEFAULT NULL,
+  `migration_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logs` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `rolled_back_at` datetime(3) DEFAULT NULL,
+  `started_at` datetime(3) NOT NULL DEFAULT current_timestamp(3),
+  `applied_steps_count` int(10) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `_prisma_migrations`
+--
+
+INSERT INTO `_prisma_migrations` (`id`, `checksum`, `finished_at`, `migration_name`, `logs`, `rolled_back_at`, `started_at`, `applied_steps_count`) VALUES
+('131b1fa3-6d44-4ec8-8347-47133c5038f0', '9fccbe2bf76b8198673651eb8dd821863a5d40a77cc4ff66548748c6296843f5', '2024-06-05 15:14:58.001', '20240605151457_init', NULL, NULL, '2024-06-05 15:14:57.551', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bets`
+--
+ALTER TABLE `bets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Bets_event_id_fkey` (`event_id`),
+  ADD KEY `Bets_team_id_fkey` (`team_id`);
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Countries_name_key` (`name`);
+
+--
+-- Indices de la tabla `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Events_sport_id_fkey` (`sport_id`);
+
+--
+-- Indices de la tabla `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Permissions_type_key` (`type`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Roles_rol_name_key` (`rol_name`);
+
+--
+-- Indices de la tabla `rolespermissions`
+--
+ALTER TABLE `rolespermissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `RolesPermissions_role_id_permission_id_key` (`role_id`,`permission_id`),
+  ADD KEY `RolesPermissions_permission_id_fkey` (`permission_id`);
+
+--
+-- Indices de la tabla `sports`
+--
+ALTER TABLE `sports`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Sports_name_key` (`name`);
+
+--
+-- Indices de la tabla `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Teams_name_key` (`name`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `Users_email_key` (`email`),
+  ADD UNIQUE KEY `Users_username_key` (`username`),
+  ADD UNIQUE KEY `Users_remember_token_key` (`remember_token`),
+  ADD KEY `Users_role_id_fkey` (`role_id`),
+  ADD KEY `Users_country_id_fkey` (`country_id`);
+
+--
+-- Indices de la tabla `userstransactions`
+--
+ALTER TABLE `userstransactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UsersTransactions_user_id_fkey` (`user_id`),
+  ADD KEY `UsersTransactions_category_id_fkey` (`category_id`),
+  ADD KEY `UsersTransactions_bet_id_fkey` (`bet_id`);
+
+--
+-- Indices de la tabla `_prisma_migrations`
+--
+ALTER TABLE `_prisma_migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bets`
+--
+ALTER TABLE `bets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `rolespermissions`
+--
+ALTER TABLE `rolespermissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `sports`
+--
+ALTER TABLE `sports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `userstransactions`
+--
+ALTER TABLE `userstransactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bets`
+--
+ALTER TABLE `bets`
+  ADD CONSTRAINT `Bets_event_id_fkey` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Bets_team_id_fkey` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `Events_sport_id_fkey` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `rolespermissions`
+--
+ALTER TABLE `rolespermissions`
+  ADD CONSTRAINT `RolesPermissions_permission_id_fkey` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `RolesPermissions_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `Users_country_id_fkey` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Users_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `userstransactions`
+--
+ALTER TABLE `userstransactions`
+  ADD CONSTRAINT `UsersTransactions_bet_id_fkey` FOREIGN KEY (`bet_id`) REFERENCES `bets` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `UsersTransactions_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `UsersTransactions_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
