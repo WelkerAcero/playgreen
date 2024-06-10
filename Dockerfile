0f37e35980d056673a5f995dbfdbe7611e61731c
WelkerAcero <universityestudiosca@gmail.com>
2024-06-10 07:16:01 -0500

    Adding Dockerfile details

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:18.20.2-alpine
+
+# Instalación de dependencias globales
+RUN npm install -g ts-node
+# Instalar PostgreSQL client
+RUN apk --no-cache add mysql-client
+
+WORKDIR /app
+
+COPY package*.json ./
+COPY . .
+#COPY docker.env .env
+
+# Instalación de dependencias del proyecto
+RUN npm install
+RUN npm run ts-config
+RUN npm run dbgenerate
+
+COPY init_db.sh /app/init_db.sh
+RUN chmod +x /app/init_db.sh
+
+CMD ["sh", "/app/init_db.sh"]
---
+++ init_db.sh
@@ -0,0 +1,14 @@
+# Esperar a que el servicio de base de datos esté disponible
+until mysqladmin ping -h roundhouse.proxy.rlwy.net -P 53007 --silent; do
+  echo "Esperando a que la base de datos esté disponible..."
+  sleep 2
+done
+
+# Ejecutar migraciones
+npm run migrate
+
+# Importar datos SQL
+mysql -h roundhouse.proxy.rlwy.net -P 53007 -u root -p fTgxaNxXNWwArAMqWhpAeFkUjiTSWJJY railway < /app/Database/sportsbook.sql
+
+# Iniciar la aplicación
+npm run start
